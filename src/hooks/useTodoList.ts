0ba6a076fcd6d9fe44f65e3d467abe314f867701
esmacbook <es250528@gmail.com>
2022-04-19 21:54:27 +0800

    crud

--- src/hooks/useTodoList.ts
+++ src/hooks/useTodoList.ts
@@ -3,7 +3,9 @@ import { Todo } from '../types/todoList'
 
 const useTodoList = () => {
     const [todos, setTodos] = useState<Todo[]>([])
+    const [filterDoneTodo, setFilterDoneTodo] = useState<boolean>(false)
 
+    // 新增
     const addTodo = (todo: Todo) => {
         setTodos([
             ...todos,
@@ -11,13 +13,18 @@ const useTodoList = () => {
         ])
     }
 
+    // 完成狀態
     const SwitchTodoDoneStatus = (id: number) => {
+
+        // 尋找點選的那個id
         const targetTodoIndex = todos.findIndex(
             (todo: Todo) => todo.id === id
         )
         
+        // 新資料避免call by refrence
         const newTodos = [...todos]
 
+        // 改變那筆資料的完成狀態
         newTodos[targetTodoIndex] = {
             ...newTodos[targetTodoIndex],
             done: !newTodos[targetTodoIndex].done,
@@ -26,7 +33,27 @@ const useTodoList = () => {
         setTodos(newTodos)
     }
 
-    return { todos, addTodo, SwitchTodoDoneStatus } as const
+    // 刪除
+    const deleteTodo = (id: number) => {
+        const newTodos = todos.filter((todo: Todo) => (
+            todo.id !== id
+        ))
+        
+        setTodos(newTodos)
+    }
+
+    // 篩選條件
+    const switchFilterDoneTodo = () => {
+        setFilterDoneTodo(!filterDoneTodo)
+    }
+
+    let workTodos = todos
+    if (filterDoneTodo) {
+        workTodos = todos.filter(todo => !todo.done)
+    }
+
+
+    return { todos: workTodos, addTodo, SwitchTodoDoneStatus, deleteTodo, filterDoneTodo, switchFilterDoneTodo } as const
 }
 
 export default useTodoList
